e9fe4b6793cdf6d3d3f182d0b67c83c5572f751d
Saviovd <savio.pinheiroal@gmail.com>
2024-12-19 23:19:07 -0400

    👌 IMPROVE: send data base to cloud

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,10 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  uri: `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 });
 
 export default pool;
