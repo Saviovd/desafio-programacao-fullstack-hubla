4821a843e5a8e187d22595a41b2b2d9e93faccf9
Saviovd <savio.pinheiroal@gmail.com>
2024-12-21 11:22:58 -0400

    📦 NEW: check if the usage is authenticated and redirect to the right screen

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,22 @@
-import Image from "next/image";
+"use client"
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import Cookie from "js-cookie";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = Cookie.get("auth_token");
+
+    if (!token) {
+      router.push("/login");
+    }
+    if (token) {
+      router.push("/dashboard");
+    }
+  }, [router]);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
