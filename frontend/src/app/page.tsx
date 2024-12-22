5a8854330a93fdd4f9527860734d35cae5ac6c70
Saviovd <savio.pinheiroal@gmail.com>
2024-12-22 14:32:35 -0400

    👌 IMPROVE: improving initial page with loading

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,8 @@
-"use client"
+"use client";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import Cookie from "js-cookie";
+import Signature from "@/components/Signature";
 
 export default function Home() {
   const router = useRouter();
@@ -20,11 +21,9 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Hello World</h1>
+        <h1>Loading...</h1>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        initial page
-      </footer>
+      <Signature /> 
     </div>
   );
 }
